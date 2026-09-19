f2badb17f5bc799f810ce0d2988952332f873736
sprajtzero <sprajtzero@gmail.com>
2026-09-20 01:13:29 +0200

    style: add and use constants in css for dark theme

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const darkModeButton = document.getElementById("dark-mode-button");
 darkModeButton.addEventListener('click', () => {
-  document.body.classList.toggle("dark-mode");
+  document.documentElement.classList.toggle("dark-mode");
 });
 
 const sliderOne = document.getElementById("slider-line-1");
