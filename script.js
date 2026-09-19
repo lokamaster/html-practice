bfa6ab4093a60ba5aee0328c268cf8cd8a2b7da5
sprajtzero <sprajtzero@gmail.com>
2026-09-19 15:41:15 +0200

    feat: add curve-slider section

--- script.js
+++ script.js
@@ -2,3 +2,21 @@ const darkModeButton = document.getElementById("dark-mode-button");
 darkModeButton.addEventListener('click', () => {
 	document.body.classList.toggle("dark-mode");
 });
+
+const sliderOne = document.getElementById("slider-line-1");
+sliderOne.addEventListener("input", (e) => {
+	const max = sliderOne.max;
+	const min = sliderOne.min;
+
+	const path = document.getElementById("path-1");
+	const circle = document.getElementById("circle-1");
+
+	const length = path.getTotalLength();
+	const pointOnPath = (
+		length * (sliderOne.value - min) / (max - min)
+	);
+	const newPoint = path.getPointAtLength(pointOnPath);
+
+	circle.setAttribute("cx", newPoint.x);
+	circle.setAttribute("cy", newPoint.y);
+});
